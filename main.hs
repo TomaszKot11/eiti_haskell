108797f3845868f14f71bebb811a1822e5b26902
Tomasz Kot <kottomasz98@gmail.com>
2020-05-23 23:10:24 +0200

    Correct bugs

--- main.hs
+++ main.hs
@@ -73,7 +73,7 @@ findLatinSqs xs = findLatinSqs' 1 [xs] where
 --                   listSub y 
 --                   nestedSub ys
 
-tower_heights = [[3, 0, 1, 0], [0, 3, 0, 0], [0, 0, 0, 0], [0, 4, 0, 0]]
+tower_heights = [[3, 0, 1, 0], [0, 3, 0, 0], [0, 0, 0, 0], [0, 0, 4, 0]]
 
 witam = [[2, 1, 4, 3], [3, 4, 2, 1], [1, 2, 3, 4], [4, 3, 1, 2]]
 enumerate = zip [0..]
@@ -97,6 +97,9 @@ papa = [(y, x, tile) | (y, row) <- enumerate tower_heights, (x, tile) <- enumera
 isSorted :: (Ord a) => [a] -> Bool
 isSorted xs = all id . map (\(x,y) -> x < y) . zip xs $ tail xs
 
+isSorted' :: (Ord a) => [a] -> Bool
+isSorted' xs = all id . map(\(x, y) -> x > y) . zip xs $ tail xs
+
 -- przyjmuje wektor zindeksowanych elementow macierzy, wektor do sprawdzenia
 -- funckja_spr :: [(Int, Int, Int)] -> [(Int, Int, Int)] -> [Int]
 -- funkcja_spr el_m
@@ -114,21 +117,22 @@ wybierz_bazujac_na_warunku (x, y, el) matrix | x == 0 = (el, (transpose matrix !
 -- elementow tzn o indekach {-1, 0}, {1, 0} autor pod indeksem -1 rozumie ideks "sztucznego" elementu o wartosci 0 wystepujacego
 -- przed elementem pierwszym - wysokosc pierwszej piramidy, nastepnie  nalezy znalezc ilosc dodatnich elementow po czym porownac 
 -- czy jest ona rowna el - jesli jest tzn. wektor jest prawidlowy
-sprawdz_czy_spelnia_warunek :: (Integer, [Integer]) -> Bool 
-sprawdz_czy_spelnia_warunek (el, lista) | el == 1 = fromIntegral(head(lista)) == n
-                                        | fromIntegral(el) == n = isSorted lista
-                                        | otherwise = sprawdz_roznice_wysokosci lista
+sprawdz_czy_spelnia_warunek :: (Integer, [Integer]) -> ()
+sprawdz_czy_spelnia_warunek (el, lista) | el == 1 && not(fromIntegral(head(lista)) == n) = error "To nie rozwiazanie 1"
+                                        | el == 1 = ()
+                                        | fromIntegral(el) == n && (not(isSorted lista) && not(isSorted' lista)) = error "To nie jest rozwiaznie 2"
+                                        | fromIntegral(el) == n = ()
+                                        | not(sprawdz_roznice_wysokosci el lista)= error "To nie jest rozwiazanie 3"
+                                        | otherwise =  ()
                            where n = length lista
 
 -- funkcja realizuja sprawdzenie roznic wysokosci odpowiednich piramid
-sprawdz_roznice_wysokosci :: [Integer] -> Bool
-sprawdz_roznice_wysokosci lista = length(filter(\n -> n > 0) final_arr) == n
+sprawdz_roznice_wysokosci :: Integer -> [Integer] -> Bool
+sprawdz_roznice_wysokosci el lista = length(filter(\n -> n > 0) final_arr) == fromIntegral(el)
        where n = length lista
              initial_arr = [lista !! 0]
-             generated = [lista !! (helper + 1) - lista !! helper  | helper <- [0..(n - 2)]]
+             generated = [(lista !! (helper + 1)) - (lista !! (helper)) | helper <- [0..(n - 2)]]
              final_arr = initial_arr ++ generated
-
-
 
 -- TODO: dodaac sprawdzanei dla 0 < el < n
 
